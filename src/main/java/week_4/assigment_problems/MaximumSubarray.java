8e2691c14a975a842fad137788d44eed4ebe14e5
ismaeelibnfarhan-wq <ismaeel.ibnfarhan@gmail.com>
2026-09-11 21:02:32 +0530

    completing function

--- src/main/java/week_4/assigment_problems/MaximumSubarray.java
+++ src/main/java/week_4/assigment_problems/MaximumSubarray.java
@@ -1,7 +1,13 @@
 package main.java.week_4.assigment_problems;
 
 public class MaximumSubarray {
-    maxSum = Math.max(maxSum, currentSum);
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
         
         return maxSum;
@@ -11,7 +17,7 @@ public class MaximumSubarray {
         int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int[] nums2 = {-3, -1, -2};
 
-       // System.out.println("Max Subarray Sum 1: " + maxSubArray(nums1)); 
-        //System.out.println("Max Subarray Sum 2: " + maxSubArray(nums2)); 
+       System.out.println("Max Subarray Sum 1: " + maxSubArray(nums1)); 
+       System.out.println("Max Subarray Sum 2: " + maxSubArray(nums2)); 
     }
 }
